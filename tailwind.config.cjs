ddafa410b9c0270b3696f91e08c4d842f02d1ffd
MobbySchiller <kb.burzynski@gmail.com>
2023-01-12 18:04:58 +0100

    Style: set tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'light-background': '#FBFBFD',
+      'light-primary-text': '#202023',
+      'light-secondary-text': '#313134',
+      'light-gray': '#424245',
+      'light-elements': '#F0F2F4',
+      'dark-background': '#151F2C',
+      'dark-primary-text': '#FDFDFD',
+      'dark-secondary-text': '#C5CBD3',
+      'dark-gray': '202023',
+      'dark-elements': '#424B57'
+    },
+    fontFamily: {
+      main: ['Source Sans Pro', 'sans-serif'],
+      title: ['EB Garamond', 'serif']
+    },
     extend: {},
   },
   plugins: [],
