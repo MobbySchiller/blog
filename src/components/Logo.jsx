9a8ed344e7ae4739108c317603bd984ac3af7ba8
MobbySchiller <kb.burzynski@gmail.com>
2023-01-13 22:32:54 +0100

    Style: change Logo styles

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
 
     return (
         <h1
-            className="text-5xl sm:text-6xl lg:text-7xl text-center font-title sm:my-12"
+            className="h-14 sm:h-20 lg:h-24 text-5xl sm:text-6xl lg:text-7xl text-center font-title sm:my-12"
             style={{
                 backgroundImage: 'linear-gradient(to bottom, #75B845 31%, #F2BE2D 31%, #F2BE2D 41%, #E09434 41%, #E09434 51%, #C55152 51%, #C55152 61%, #934E84 61%, #934E84 71%, #009EDD 71%)',
                 color: 'transparent',
