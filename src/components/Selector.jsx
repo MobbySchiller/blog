f408ac9db099f05bb5f4219c2b03cf9ebacc04a1
MobbySchiller <kb.burzynski@gmail.com>
2023-01-14 22:09:46 +0100

    Style: add Articles styles

---
+++ src/components/Selector.jsx
@@ -0,0 +1,28 @@
+const Selector = ({ name, options }) => {
+
+    const selectorOptions = options.map(option => {
+        const { id, name, value } = option
+        return (
+            <option key={id} value={value}>{name}</option>
+        )
+    })
+
+
+    return (
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+            <label class="block tracking-wide text-light-primary dark:text-dark-primary mb-1" for="grid-state">
+                {name}
+            </label>
+            <div class="relative">
+                <select class="block appearance-none w-full bg-light-elements dark:bg-dark-elements border border-light-gray dark:border-dark-secondary text-light-primary dark:text-dark-primary py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-theme-icon dark:focus:border-theme-icon">
+                    {selectorOptions}
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-light-primary dark:text-dark-primary">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Selector
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,7 +1,41 @@
+import Selector from '../components/Selector'
+import Preview from '../components/Preview'
+
 const Articles = () => {
 
+    const categoryOptions = [
+        { id: 0, name: 'JavaScript', value: 'javascript' },
+        { id: 1, name: 'TypeScript', value: 'typescript' },
+        { id: 3, name: 'Testy', value: 'tests' },
+    ]
+    const sortOptions = [
+        { id: 0, name: 'Tytuł: od A do Z', value: 'aToZ' },
+        { id: 1, name: 'Tytuł: od Z do A', value: 'zToA' },
+        { id: 2, name: 'Data: od najnowszych', value: 'newToOld' },
+        { id: 3, name: 'Data: od najstarszych', value: 'oldToNew' },
+    ]
+
     return (
-        <h2>Artykuły</h2>
+        <>
+            <section className="flex flex-col sm:flex-row justify-between my-4">
+                <Selector name={'Kategoria'} options={categoryOptions} />
+                <Selector name={'Sortuj'} options={sortOptions} />
+            </section >
+            <section className='w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-2 mx-auto'>
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+                <Preview />
+            </section>
+        </>
     )
 }
 
